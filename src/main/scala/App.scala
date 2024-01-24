e851de8fa46606f0d5a1fcc0035a5d125d7f3f6e
Maxime Dupont <m-dupont01@outlook.fr>
2024-01-24 16:30:23 +0100

    Determine main categorie of business

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,6 +5,91 @@ import org.apache.spark.sql.functions._
 import java.util.Properties
 
 object App {
+
+	// Fonction qui prend en entrée un string contenant des valeurs séparées par une virgule
+	// et retourne une des valeurs sous forme de string
+	def getMainCategorie(str_categories: String): String = {
+		// Récupération des catégories dans un tableau en supprimant les majuscules
+		var categories = str_categories.toLowerCase().split(",")
+
+		// On retourne une chaîne vide si la liste est vide
+		if (categories.length == 0) {
+			return ""
+		}
+
+		// Recherche de catégories prédéfinies
+		// Il faut que les catégories soient dans l'ordre de priorité...
+		// Les mots peuvent être présents avec des suffixes ou des préfixes
+
+		// On cherche si une des catégories est relative à la restauration / vente de nourriture ou de boissons
+		var motsRestauration = Seq("restau", "food", "drink", "bar", "cafe", "coffee", "tea", "breakfast", "brunch", "lunch", "dinner", "bistro", "pub", "brewery", "beer", "wine", "bakery", "cuisine", "caterer")
+		for (categorie <- categories) {
+			if(motsRestauration.exists(mot => categorie.contains(mot))) {
+				return "restauration"
+			}
+		}
+
+		// On cherche si une des catégories est relative à la vente et au commerce
+		val motsCommerce = Seq("shop", "store", "market", "market", "boutique", "commerce", "commercial", "retail", "sale", "buy", "purchase", "sell", "vendor", "dealer", "mall", "grocery", "pharmacy", "drugstore", "bookstore", "library", "florist")
+		for (categorie <- categories) {
+			if(motsCommerce.exists(mot => categorie.contains(mot))) {
+				return "shopping"
+			}
+		}
+
+		// On cherche si une des catégories est relative à la santé / médecine
+		val motsSante = Seq("health", "medica", "salon", "clinic", "hospital", "doctor", "dentist", "pharma", "medicin")
+		for (categorie <- categories) {
+			if(motsSante.exists(mot => categorie.contains(mot))) {
+				return "medical"
+			}
+		}
+
+		// On cherche si une des catégories est relative au soin du corps / beauté
+		val motsSoins = Seq("beauty", "spa", "hair", "nail", "tattoo", "piercing", "wax", "massage", "salon", "life")
+		for (categorie <- categories) {
+			if(motsSoins.exists(mot => categorie.contains(mot))) {
+				return "beauty"
+			}
+		}
+
+		// On cherche si une des catégories est relative à l'automobile / réparation
+		val motsAutomobile = Seq("auto", "moto", "car", "repair", "garage", "mechanic", "tire", "wheel")
+		for (categorie <- categories) {
+			if(motsAutomobile.exists(mot => categorie.contains(mot))) {
+				return "automobile"
+			}
+		}
+
+		// On cherche si une des catégories est relative aux lieux de culte
+		val motsReligion = Seq("church", "temple", "mosque", "synagogue", "religio", "spiritual", "faith", "worship", "pray")
+		for (categorie <- categories) {
+			if(motsReligion.exists(mot => categorie.contains(mot))) {
+				return "religion"
+			}
+		}
+
+		// On cherche si une des catégories est relative aux loisirs / divertissements
+		val motsLoisirs = Seq("entertainment", "fun", "leisure", "recreation", "game", "play", "sport", "gym", "hobby", "activity", "event", "party", "dance", "music", "concert", "movie", "film", "theater", "cinema", "show", "amusement", "park", "zoo", "museum", "art", "gallery", "exhibit", "exhibition", "tourism", "tourist", "travel", "voyage", "vacation", "holiday", "trip", "visit", "sightseeing", "sightsee", "attraction", "tour", "excursion", "cruise", "cruising", "carnival", "festival")
+		for (categorie <- categories) {
+			if(motsLoisirs.exists(mot => categorie.contains(mot))) {
+				return "entertainment"
+			}
+		}
+
+		// On cherche si une des catégories est relative aux services
+		val motsService = Seq("service", "event", "planning", "photo", "repair", "mobile", "electronics", "computer", "clean", "wash", "laundry", "dry", "cleaning", "delivery", "shipping", "transport", "moving", "storage", "logistic")
+		for (categorie <- categories) {
+			if(motsService.exists(mot => categorie.contains(mot))) {
+				return "service"
+			}
+		}
+		
+
+		// Si on ne trouve pas de catégorie parmi celles prédéfinies, on retourne la première
+		return categories(0)
+	}
+
 	def main(args: Array[String]) {
 
 		/************************************************
@@ -72,22 +157,8 @@ object App {
 		//reviews.limit(10).show()
 
 
-		// Recupération des données de la table "user"
-		val queryGetUsers =
-		"""
-			|SELECT user_id, review_count, yelping_since
-			|FROM yelp."user"
-			|WHERE user_id is not null
-			|LIMIT 10
-		""".stripMargin
-
-		// val users = spark.read.jdbc(urlPostgres, s"($queryGetUsers) as review", connectionPropertiesPostgres)
-
-		// affichage du schéma
-		// users.printSchema()
-
-		// recuperation de données
-		// users.limit(10).show()
+		// Les données de la table "user" sont récupérées dans la partie "Ajout de données au data warehouse - base de données Oracle"
+		// car on n'effectue pas de transformation dessus
 
 
 		/**************************************************************
@@ -123,7 +194,7 @@ object App {
 		 * Nous avons besoin d'extraire ces dates de la string. 
 		 */
 
-		// Extraction des dates, qui formeront une table "business_id - date_id"
+		// Extraction des dates, qui formeront une table "date"
 		// val first_checkins = checkins.limit(10)
 		// val visitDates = first_checkins.withColumn("date", explode(org.apache.spark.sql.functions.split(col("date"), ",")))
 
@@ -238,7 +309,30 @@ object App {
 				.drop(col("hours"))
 
 		// On affiche les 10 premières lignes
-		openDays.show(10)
+		// openDays.show(10)
+
+		// Extraction des catégories
+		val business_categories = business.select("business_id", "categories")
+		// business_categories = business_categories.withColumn("categories", explode(org.apache.spark.sql.functions.split(col("categories"), ",")))
+
+		val getMainCategorieUDF = udf(getMainCategorie _)
+
+		// On crée une colonne "main_categorie" qui contient la catégorie principale du business
+		var business_main_categorie = business_categories.withColumn("main_categorie", getMainCategorieUDF(col("categories")))
+
+		// Affichage des 10 premières lignes
+		// business_categorie.show(10)
+
+		business_main_categorie.show(100)
+
+
+		// Extraction des attributs
+		// val attributes = business.select("business_id", "attributes")
+
+
+		// Suppression des colonnes inutiles dans le dataframe "business"
+		business = business.drop(col("categories"))
+							.drop(col("attributes"))
 
 
 
@@ -247,11 +341,41 @@ object App {
 			Ajout de données au data warehouse - base de données Oracle
 		*******************************************************************/
 
-		// Enregistrement du DataFrame reviews dans la table "reviews"
-		// reviews.write.mode(SaveMode.Overwrite).jdbc(urlOracle, "reviews", connectionPropertiesOracle)
+		// Enregistrement du DataFrame date dans la table "date"
+		// dates.write.mode(SaveMode.Overwrite).jdbc(urlOracle, "date", connectionPropertiesOracle)
+		// Libération de la mémoire
+		// dates.unpersist()
 
 
-		
+		// Enregistrement du DataFrame reviews dans la table "review"
+		// reviews.write.mode(SaveMode.Overwrite).jdbc(urlOracle, "review", connectionPropertiesOracle)
+		// Libération de la mémoire
+		// reviews.unpersist()
+
+
+		// Recupération des données de la table "user" depuis la base de données Postgres
+		// et enregistrement dans la table "users" de la base de données Oracle
+		val queryGetUsers =
+		"""
+			|SELECT user_id, review_count, yelping_since
+			|FROM yelp."user"
+			|WHERE user_id is not null
+			|LIMIT 10
+		""".stripMargin
+
+		// val users = spark.read.jdbc(urlPostgres, s"($queryGetUsers) as review", connectionPropertiesPostgres)
+
+		// affichage du schéma
+		// users.printSchema()
+
+		// recuperation de données
+		// users.limit(10).show()
+
+		// Enregistrement du DataFrame user dans la table "user"
+		// users.write.mode(SaveMode.Overwrite).jdbc(urlOracle, "user", connectionPropertiesOracle)
+		// Libération de la mémoire
+		// users.unpersist()
+
 
 
 		// Arrêt de la session Spark
